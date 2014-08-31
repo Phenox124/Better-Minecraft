74c3bab8cff6f6ddf3be52b7e6acbdc5958f9ac3
Phenox124 <blyall44@gmail.com>
2014-08-31 17:17:43 -0600

    Just starting up

---
+++ Src/Main/Java/com/phenox/betterminecraft/betterminecraft.java
@@ -0,0 +1,32 @@
+package com.phenox.betterminecraft;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "betterminecraft", name = "Better Minecraft", version = "1.7.10-1.0")
+public class betterminecraft
+{
+
+    @Mod.Instance("betterminecraft")
+    public static betterminecraft instance;
+
+    @Mod.EventHandler
+    public void preinit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void post(FMLPostInitializationEvent evet)
+    {
+
+    }
+}
